0e338ef1a6b51be73a6f7c2a85f66669d206ba2a
Dawid Kamasz <dawid.kamasz@gmail.com>
2019-03-10 14:17:09 +0100

    metoda wyswietlajaca pogode w danym miescie

--- src/main/java/pl/sda/weather/Condition.java
+++ src/main/java/pl/sda/weather/Condition.java
@@ -1,4 +1,14 @@
 package pl.sda.weather;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class Condition {
+    private String text;
+    private String icon;
+    private int code;
 }
--- src/main/java/pl/sda/weather/Weather.java
+++ src/main/java/pl/sda/weather/Weather.java
@@ -1,4 +1,13 @@
 package pl.sda.weather;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Weather {
+    Location location;
+    Current current;
 }
